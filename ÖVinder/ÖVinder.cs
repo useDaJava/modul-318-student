390df4a5190419e4e3c953be90110b9645ea7be2
dok515 <robin@3egg.ch>
2016-05-25 09:35:32 +0200

    Add DateTime Conversion

--- ÖVinder/ÖVinder.cs
+++ ÖVinder/ÖVinder.cs
@@ -41,12 +41,14 @@ namespace ÖVinder
             tableLayoutPanelVerbindungen.Controls.Add(new Label() { Text = "Dauer:" }, columnCount++, rowCount);
             tableLayoutPanelVerbindungen.Controls.Add(new Label() { Text = "Gleis / Platform:" }, columnCount++, rowCount);
             rowCount++;
+            DateTime departureTime;
             foreach (Connection targetConnection in connections.ConnectionList) {
                 columnCount = 0;
-                Console.WriteLine(targetConnection.From.Station.Name.ToString());
-                Console.WriteLine(targetConnection.To.Station.Name.ToString());
-                tableLayoutPanelVerbindungen.Controls.Add(new Label() { Text = targetConnection.From.Departure.ToString() }, columnCount, rowCount);
-                tableLayoutPanelVerbindungen.Controls.Add(new Label() { Text = targetConnection.Duration.ToString() }, columnCount++, rowCount);
+                Console.WriteLine(targetConnection.Duration.ToString());
+                departureTime = Convert.ToDateTime(targetConnection.From.Departure.ToString());
+                string duration = targetConnection.Duration.ToString().Substring(targetConnection.Duration.ToString().Length - 7);
+                tableLayoutPanelVerbindungen.Controls.Add(new Label() { Text = departureTime.Hour.ToString() + ":" + departureTime.Minute.ToString() }, columnCount, rowCount);
+                tableLayoutPanelVerbindungen.Controls.Add(new Label() { Text = duration }, columnCount++, rowCount);
                 tableLayoutPanelVerbindungen.Controls.Add(new Label() { Text = targetConnection.From.Platform }, columnCount++, rowCount);
                 rowCount += 1;
             }            
